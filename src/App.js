96058d82462d08d5887f2525f5cef4e3c45e0ce4
Himanshu-1433 <himanshu1433@tnraocollege.org>
2022-12-14 11:47:20 +0530

    asdf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React , {useState} from 'react';
+import React , {useState , useEffect} from 'react';
 import './App.css';
 
 function App() {
   
   const [check , checking] = useState("hello");
   const [name , Change_name] = useState("hola")
+  useEffect(() => {
+    // Update the document title using the browser API
+    alert(`you are changing ${check}`);
+  });
   return (
     <div className="App">
       <h1>{check}</h1>
